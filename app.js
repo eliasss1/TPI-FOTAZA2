a8a6a044ab73bda3100e9b7c420cef422563635f
Elias Pereyra <elias13pereyra@gmail.com>
2026-06-12 18:57:13 -0300

    arreglo de sessiones y db para supabase y vercel

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require('express');
 const path = require('path');
 const authRoutes = require('./src/routes/authRoutes');
 const postRoutes = require('./src/routes/postRoutes');
+
 const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const { Pool } = require('pg');
+
+const sequelize = require('./src/config/database'); 
 
 // Iniciar la app y configurar vistas.
 const app = express(); 
@@ -14,12 +19,28 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 
 // MIDDLEWARES 
 app.use(express.urlencoded({extended : true}));
-app.use(express.json())
+app.use(express.json());
+
+const dbPool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
 
 app.use(session({
+    store: new pgSession({
+        pool: dbPool,               
+        tableName: 'session',        
+        createTableIfMissing: true   
+    }),
     secret: process.env.SESSION_SECRET || 'clave_secreta',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false,        
+    cookie: { 
+        maxAge: 30 * 24 * 60 * 60 * 1000 
+    }
 }));
 
 app.use((req, res, next) => {
@@ -29,7 +50,6 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'src','public')));
 
-
 // RUTAS
 app.use('/', postRoutes);
 
@@ -37,19 +57,17 @@ app.use('/', postRoutes);
 app.use('/auth', authRoutes);
 
 // MIDDLEWARES RUTAS SUELTAS
-
 const {estaAutenticado, esModerador} = require('./src/middlewares/authMiddleware');
 
 // Iniciar el servidor
+const Port = process.env.PORT || 3000; 
 
-
-const Port = process.env.Port || 3000;
 sequelize.sync({ alter: true })
     .then(() => {
-    console.log('Tablas sincronizadas con éxito');
+        console.log('Tablas sincronizadas con éxito');
     })
     .catch((error) => {
-    console.error('Error al sincronizar las tablas:', error);
+        console.error('Error al sincronizar las tablas:', error);
     });
 
 app.listen(Port, () => {
